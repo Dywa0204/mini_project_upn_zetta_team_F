ee6899ad52b142ad191e0d132e55d9a217fd829d
Dywa0204 <dywa.pratama0204@gmail.com>
2022-11-30 14:24:08 +0700

    Update Routing : Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: "", loadChildren: () => import('./home/home.module').then(m => m.HomeModule)}, 
-  {path: "login", loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule)},
-  {path: "menu", loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: "menu", loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule)},
+  {path: "login", loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
   {path: "cart", loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)}
 ];
 
