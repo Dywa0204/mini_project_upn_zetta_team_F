201ae55a7f95182008336cca92ab4c67eee7f3c7
Dywa0204 <dywa.pratama0204@gmail.com>
2022-11-30 13:52:25 +0700

    Update Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: "", loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path: "", loadChildren: () => import('./home/home.module').then(m => m.HomeModule)}, 
   {path: "login", loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule)},
   {path: "menu", loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
   {path: "cart", loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)}
