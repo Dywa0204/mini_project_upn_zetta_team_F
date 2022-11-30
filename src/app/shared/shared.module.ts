22384df58827cc6508b8a85cc13b95a1b09739d7
Dywa0204 <dywa.pratama0204@gmail.com>
2022-11-30 13:51:40 +0700

    Update UI : Add Navigation

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+
+import {} from '@angular/material'
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ]
+})
+export class SharedModule { }
